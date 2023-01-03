e388ae82692b18708e02e0ed9be82f678bb23d08
Chris Garcia <garciac9203@gmail.com>
2023-01-03 15:01:02 -0800

    moved loader route

--- server/index.js
+++ server/index.js
@@ -11,10 +11,6 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.get(`/${process.env.LTOKEN}`, (req, res) => {
-  res.send(`${process.env.LTOKEN}`);
-});
-
 app.use(router);
 
 app.listen(process.env.PORT, (err) => {
--- server/routes.js
+++ server/routes.js
@@ -6,5 +6,8 @@ router.get('/products', controllers.getAllProducts)
 router.get('/products/:product_id', controllers.getProduct)
 router.get('/products/:product_id/styles', controllers.getStyles)
 router.get('/products/:product_id/related', controllers.getRelated)
+router.get(`/${process.env.LTOKEN}`, (req, res) => {
+  res.send(`${process.env.LTOKEN}`);
+});
 
 module.exports = router;
